b97230fa30e161a6a061e30b98822cfa87985eb0
Ankurpainyuli12 <ankurpainyuli@gmail.com>
2024-12-06 11:20:17 +0530

    updated

--- backend/modals/messageModel.js
+++ backend/modals/messageModel.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import mongoose from 'mongoose';
 
 const messageSchema = new mongoose.Schema({
@@ -12,26 +11,3 @@ const messageSchema = new mongoose.Schema({
 const Message = mongoose.model('Message', messageSchema);
 
 export default Message;
-=======
-import mongoose from "mongoose";
-
-const messageSchema = new mongoose.Schema(
-  {
-    senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    content: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-    landId: { type: mongoose.Schema.Types.ObjectId, ref: 'Land', required: true },
-    replies: [
-      {
-        replyContent: { type: String, required: true },
-        senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-        createdAt: { type: Date, default: Date.now },
-      },
-    ],
-  },
-  { timestamps: true }
-);
-
-const Message = mongoose.model('Message', messageSchema);
-export default  Message;
->>>>>>> fee9ba12695b5b8fe15a6179bfea51c7ad557344
